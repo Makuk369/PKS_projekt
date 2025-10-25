8cd614a50366516396f0583a42d4373ebe5c1029
Makuk369 <makuk369@gmail.com>
2025-10-25 11:05:45 +0200

    json msg setup done

--- Message.py
+++ Message.py
@@ -5,8 +5,7 @@ from enum import Enum
 from Sensors import SensorType
 
 class MessageType(Enum):
-    REGISTRATION = 0
-    DEREGISTRATION = 1
+    REG = 0
 
 def JsonSerializer(obj):
     if isinstance(obj, Enum):
@@ -19,35 +18,24 @@ def JsonSerializer(obj):
 class Message():
     sensorType: SensorType
     msgType: MessageType
-    timestamp: datetime = field(default=datetime.now())
-    battery: int = field(default=100)
     token: int = field(default=-1)
+    battery: int = field(default=100)
+    timestamp: datetime = field(default=datetime.now())
+    data: list = field(default_factory=list) #additional data
 
     def ToJson(self) -> None:
         with open("msg.json", "w") as f:
             json.dump(asdict(self), f, default=JsonSerializer, indent=4)
 
-    # @abstractmethod
     def FromJson(self) -> dict:
         with open("msg.json", "r") as f:
             data = json.load(f)
             return data
-
-# @dataclass
-# class RegistrationMsg(Message):
-#     def __post_init__(self) -> None:
-#         self.msgType = MessageType.REGISTRATION
-
-    # def ToJson(self) -> None:
-    #     with open("msg.json", "w") as f:
-    #         json.dump(asdict(self), f, default=JsonSerializer, indent=4)
-
-    # def FromJson(self) -> None:
-    #     pass
     
 
 if __name__ == '__main__':
-    msg = Message(SensorType.THERMONODE, MessageType.REGISTRATION)
+    msg = Message(SensorType.THERMONODE, MessageType.REG)
+    # msg.data = ["daco", 123, 3.14]
     msg.ToJson()
     rcv = msg.FromJson()
     print(rcv)
